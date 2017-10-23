c2f0a42f60243387f18e48d72d1d37c05e3a986f
NickR23 <nickrich2624@gmail.com>
2017-10-23 12:00:25 -0400

    position electron correctly

--- src/DrawingComponent.java
+++ src/DrawingComponent.java
@@ -12,19 +12,22 @@ public class DrawingComponent extends JPanel implements ActionListener{
 
 	private Timer worldT = new Timer (100,this);
 	float angle = 20;
-	int ex = 120, ey = 120;
+	double ex = 230, ey = 190;
 	
 	
 	//Paints to JFrame
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;
+		//enables antialiasing and pure stroke
+	    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	    g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
 		
-		setBackground(Color.BLACK);
+	    setBackground(Color.BLACK);
 		g2d.setColor(Color.WHITE);
 		g2d.fillOval(150, 150, 50, 50);
 		g2d.setColor(Color.YELLOW);
-		g2d.fillOval(ex, ey, 5, 5);
+		g2d.fillOval((int)ex, (int)ey, 5, 5);
 		//init scene timer
 		worldT.start();
 		
@@ -32,8 +35,8 @@ public class DrawingComponent extends JPanel implements ActionListener{
 	
 	
 	public void actionPerformed(ActionEvent e) {
-		ex = (ex -= 10*Math.cos(angle));
-		ey = (ey += 10*Math.sin(angle));
+		ex = (ex - 30*Math.cos(angle));
+		ey = (ey + 30*Math.sin(angle));
 		angle-=0.5;
 		repaint();
 	}
