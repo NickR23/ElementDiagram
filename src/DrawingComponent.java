aac7917377b9250a08ee213ecc01e03abf4709e8
NickR23 <nickrich2624@gmail.com>
2017-10-23 18:50:10 -0400

    Add ability to handle more than one element

--- src/DrawingComponent.java
+++ src/DrawingComponent.java
@@ -6,23 +6,28 @@ import java.io.IOException;
 import java.io.File;
 import java.util.Scanner;
 import java.lang.Math;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class DrawingComponent extends JPanel implements ActionListener 
+public class DrawingComponent extends JPanel implements ActionListener, MouseListener 
 {
 
 	private Timer worldT = new Timer (100,this);
 	private float angle = 20;
 	private double ex = (int)(getWidth()/2)+675, ey = (int)(getHeight()/2)+225;
-	private int numOfElements = 1;
-    private Element element = new Element();
+	private int numOfElements = 2;
+	private int currentElement = 1;
+    private Element[] elements = new Element[numOfElements];
 	File file = new File("Elements.txt");
 	
     public void dataLoad() throws IOException{
 		Scanner inFile = new Scanner(file);
-		//element = new Element();
-		element.setElectrons(inFile.nextInt());
-		element.setProtons(inFile.nextInt());
-		element.setName(inFile.nextLine());
+		for(int i=0; i< numOfElements;i++) {
+			elements[i] = new Element();
+			elements[i].setElectrons(inFile.nextInt());
+			elements[i].setProtons(inFile.nextInt());
+			elements[i].setName(inFile.nextLine());
+		}
 		
 		inFile.close();
 		System.out.println("Data Loaded");
@@ -49,9 +54,10 @@ public class DrawingComponent extends JPanel implements ActionListener
 		g2d.fill(electron);
 		
 		//displays selected element data
-		g2d.drawString("Current element: " + element.getName(), 500, 500);
-		g2d.drawString("Number of protons: " + element.getProtons(), 500, 515);
-		g2d.drawString("Number of electrons: " + element.getElectrons(), 500, 530);
+		g2d.drawString("Current element: " + elements[currentElement].getName(), 500, 500);
+		g2d.drawString("Number of protons: " + elements[currentElement].getProtons(), 500, 515);
+		g2d.drawString("Number of electrons: " + elements[currentElement].getElectrons(), 500, 530);
+		g2d.drawString("Electron postion(s): " + ex + "," + ey, 500, 545);
 
         //Creates a "text console"
         g2d.setColor(Color.WHITE);
@@ -66,5 +72,45 @@ public class DrawingComponent extends JPanel implements ActionListener
 		ey = ey + 30 * Math.sin(angle);
 		angle-=0.5;
 		repaint();
+	}
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
